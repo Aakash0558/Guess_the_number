71b5343ae718da1dc1f200cbdc0eb4a33b6d7411
Aakash Mishra <65229546+Aakash0558@users.noreply.github.com>
2021-03-02 17:04:09 +0530

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,42 +1,11 @@
 import random
 from art import logo
 
-def easy_mode(number, mode):
-  #function for easy mode
-  attempts = 10
-  while attempts != 0:
-    print(f"You have {attempts} attempts remaining to guess the number.")
-    guess = int(input("Make a guess: "))
-    if guess > 100 or guess < 1:
-      #for out of bounds guesses
-      print("Your guess is out of bounds, you lose one attempt.")
-      attempts -= 1
-      if attempts == 0:
-        print("You've run out of guesses, you lose.")
-      else:
-        print("Guess again.")
-    elif guess > number:
-      print("Too high.")
-      attempts -= 1
-      if attempts == 0:
-        print("You've run out of guesses, you lose.")
-      else:
-        print("Guess again.")
-    elif guess < number:
-      print("Too low.")
-      attempts -= 1
-      if attempts == 0:
-        print("You've run out of guesses, you lose.")
-      else:
-        print("Guess again.")
-    elif guess == number:
-      print(f"You got it! The answer was {guess}.")
-      attempts = 0
-
-
-def hard_mode(number, mode):
-  #function for hard mode
-  attempts = 5
+def game_mode(number, mode):
+  if mode == "easy":
+    attempts = 10
+  else:
+    attempts = 5
   while attempts != 0:
     print(f"You have {attempts} attempts remaining to guess the number.")
     guess = int(input("Make a guess: "))
@@ -80,10 +49,10 @@ game_running = True
 while game_running is True:
   mode = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
   if mode == "easy":
-    easy_mode(number, mode)
+    game_mode(number, mode)
     game_running = False
   elif mode == "hard":
-    hard_mode(number, mode)
+    game_mode(number, mode)
     game_running = False
   else:
     print("Your input is wrong, try again!")
